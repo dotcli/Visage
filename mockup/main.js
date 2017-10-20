d4ae5589bfdf3792d6bcae471a649cef3452c5fe
Changbai Li <changbai.li@heliosinteractive.com>
2017-10-20 14:52:51 -0700

    Create svg mockup for Visage

---
+++ mockup/main.js
@@ -0,0 +1,49 @@
+// click to turn off / on
+
+// display indices!
+
+var jewel1 = []
+// TODO
+var jewel2 = []
+var jewel3 = []
+var jewels = [
+  [],
+  [],
+  []
+]
+
+var jewelRadius = 40
+var jewelAngle = Math.PI / 3
+
+var triLength = 300
+
+function arrangePixels(pixel, iJewel, iPixel) {
+  var pixelCenters = [
+    [100, 100],
+    [100 + triLength, 100],
+    [100 + (triLength / 2), 100 + triLength * Math.sin(Math.PI / 3)],
+  ]
+  if (iPixel === 0) {
+    pixel.setAttribute('cx', pixelCenters[iJewel][0])
+    pixel.setAttribute('cy', pixelCenters[iJewel][1])
+    return;
+  }
+  var angle = jewelAngle * (iPixel - 1)
+  var cx = pixelCenters[iJewel][0] + Math.cos(angle) * jewelRadius
+  var cy = pixelCenters[iJewel][1] + Math.sin(angle) * jewelRadius
+  pixel.setAttribute('cx', cx)
+  pixel.setAttribute('cy', cy)
+}
+
+var allPixels = document.querySelectorAll('circle')
+
+for (var i = 0; i < allPixels.length; i++) {
+  var p = allPixels[i];
+  var iJewel = Math.floor(i / 7)
+  var iPixel = i % 7
+
+  p.dataset.iJewel = iJewel
+  p.dataset.iPixel = iPixel
+  arrangePixels(p, iJewel, iPixel)
+  jewels[iJewel].push(p)
+}
