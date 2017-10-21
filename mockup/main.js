187ef1125d8413f0bcc0b5861e9cc3c05a0a11e6
Changbai Li <changbai.li@heliosinteractive.com>
2017-10-20 17:26:52 -0700

    add eslint

--- mockup/main.js
+++ mockup/main.js
@@ -2,57 +2,57 @@
 
 // display indices!
 
-var jewel1 = []
+var jewel1 = [];
 // TODO
-var jewel2 = []
-var jewel3 = []
+var jewel2 = [];
+var jewel3 = [];
 var jewels = [
   [],
   [],
   []
-]
+];
 
-var jewelRadius = 40
-var jewelAngle = Math.PI / 3
+var jewelRadius = 40;
+var jewelAngle = Math.PI / 3;
 
-var triLength = 300
+var triLength = 400;
 
 function arrangePixels(pixel, iJewel, iPixel) {
   var pixelCenters = [
     [100, 100],
     [100 + triLength, 100],
     [100 + (triLength / 2), 100 + triLength * Math.sin(Math.PI / 3)],
-  ]
+  ];
   if (iPixel === 0) {
-    pixel.setAttribute('cx', pixelCenters[iJewel][0])
-    pixel.setAttribute('cy', pixelCenters[iJewel][1])
+    pixel.setAttribute('cx', pixelCenters[iJewel][0]);
+    pixel.setAttribute('cy', pixelCenters[iJewel][1]);
     return;
   }
-  var angle = jewelAngle * (iPixel - 1 + 0.5)
-  var cx = pixelCenters[iJewel][0] + Math.cos(angle) * jewelRadius
-  var cy = pixelCenters[iJewel][1] + Math.sin(angle) * jewelRadius
-  pixel.setAttribute('cx', cx)
-  pixel.setAttribute('cy', cy)
+  var angle = jewelAngle * (iPixel - 1 + 0.5);
+  var cx = pixelCenters[iJewel][0] + Math.cos(angle) * jewelRadius;
+  var cy = pixelCenters[iJewel][1] + Math.sin(angle) * jewelRadius;
+  pixel.setAttribute('cx', cx);
+  pixel.setAttribute('cy', cy);
 }
 
-var allPixels = document.querySelectorAll('circle')
+var allPixels = document.querySelectorAll('circle');
 
 for (var i = 0; i < allPixels.length; i++) {
   var p = allPixels[i];
-  var iJewel = Math.floor(i / 7)
-  var iPixel = i % 7
+  var iJewel = Math.floor(i / 7);
+  var iPixel = i % 7;
 
-  p.dataset.iJewel = iJewel
-  p.dataset.iPixel = iPixel
-  arrangePixels(p, iJewel, iPixel)
-  p.addEventListener('click', clickSwitchFill)
-  jewels[iJewel].push(p)
+  p.dataset.iJewel = iJewel;
+  p.dataset.iPixel = iPixel;
+  arrangePixels(p, iJewel, iPixel);
+  p.addEventListener('click', clickSwitchFill);
+  jewels[iJewel].push(p);
 }
 
 function clickSwitchFill(e) {
-  var fill = e.target.getAttribute('fill')
-  if (fill === 'black') e.target.setAttribute('fill', 'white')
-  else if (fill === 'white') e.target.setAttribute('fill', 'black')
+  var fill = e.target.getAttribute('fill');
+  if (fill === 'black') e.target.setAttribute('fill', 'white');
+  else if (fill === 'white') e.target.setAttribute('fill', 'black');
 }
 
 function fill() {
