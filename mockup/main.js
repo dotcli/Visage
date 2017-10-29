b41cefb31d2a2c9aa2a9dbe621215fe739d205ad
lcb931023 <lcb931023@gmail.com>
2017-10-28 20:47:26 -0700

    Refactor mockup app using vue; generate python code for colors

--- mockup/main.js
+++ mockup/main.js
@@ -1,60 +1,82 @@
-// click to turn off / on
+// transform into vue!
+var JEWEL_COUNT = 3;
+var PIXELS_EACH_JEWEL = 7;
+var PIXEL_COUNT = JEWEL_COUNT * PIXELS_EACH_JEWEL;
+var STARTER_COLOR = 'white';
 
-// display indices!
+new Vue({
+  el: '#app',
+  data: {
+    jewelRadius: 40,
+    jewelAngle: Math.PI / 3,
+    triLength: 400,
+    pixels: [], // initialize in created hook
+  },
+  computed: {
+    pixelCenters: function() {
+      return [
+        [100, 100],
+        [100 + this.triLength, 100],
+        [100 + (this.triLength / 2), 100 + this.triLength * Math.sin(Math.PI / 3)],
+      ];
+    },
+    pythonCode: function() {
+      var pixelColors = this.pixels.map(function(p) {
+        var color = tinycolor(p.fill);
+        if (color.isValid() === false) throw new Error('pixel fill is not valid color', p.fill);
+        return [color._r, color._g, color._b];
+      });
+      return JSON.stringify(pixelColors);
+    }
+  },
+  created: function() {
+    for (var i = 0; i < PIXEL_COUNT; i++) {
+      var p = {
+        fill: STARTER_COLOR,
+        x: 0,
+        y: 0,
+      };
 
-var jewel1 = [];
-// TODO
-var jewel2 = [];
-var jewel3 = [];
-var jewels = [
-  [],
-  [],
-  []
-];
+      var iJewel = Math.floor(i / PIXELS_EACH_JEWEL);
+      var iPixel = i % 7;
+      this.arrangePixels(p, iJewel, iPixel);
+      this.pixels[i] = p;
+    }
+  },
+  methods: {
+    arrangePixels: function (pixel, iJewel, iPixel) {
+      if (iPixel === 0) {
+        pixel.x = this.pixelCenters[iJewel][0];
+        pixel.y = this.pixelCenters[iJewel][1];
+        return;
+      }
+      var angle = this.jewelAngle * (iPixel - 1 + 0.5);
+      pixel.x = this.pixelCenters[iJewel][0] + Math.cos(angle) * this.jewelRadius;
+      pixel.y = this.pixelCenters[iJewel][1] + Math.sin(angle) * this.jewelRadius;
+    },
+    setPixelColor: function(e) {
+      // set pixel color and force render update
+      var targetIndex = Number(e.target.dataset.index);
+      var targetPixel = this.pixels[targetIndex];
+      if (targetPixel.fill === 'black') targetPixel.fill = 'white';
+      else if (targetPixel.fill === 'white') targetPixel.fill = 'black';
+      // force reactivity
+      this.$set(this.pixels, targetIndex, targetPixel);
+    }
+  },
+});
 
-var jewelRadius = 40;
-var jewelAngle = Math.PI / 3;
 
-var triLength = 400;
+/* 
+TODO make color palette creator
+- create color based on CSS value input?
+- plus and minus to manage
+- click to choose that color
+- click pixels to color them
 
-function arrangePixels(pixel, iJewel, iPixel) {
-  var pixelCenters = [
-    [100, 100],
-    [100 + triLength, 100],
-    [100 + (triLength / 2), 100 + triLength * Math.sin(Math.PI / 3)],
-  ];
-  if (iPixel === 0) {
-    pixel.setAttribute('cx', pixelCenters[iJewel][0]);
-    pixel.setAttribute('cy', pixelCenters[iJewel][1]);
-    return;
-  }
-  var angle = jewelAngle * (iPixel - 1 + 0.5);
-  var cx = pixelCenters[iJewel][0] + Math.cos(angle) * jewelRadius;
-  var cy = pixelCenters[iJewel][1] + Math.sin(angle) * jewelRadius;
-  pixel.setAttribute('cx', cx);
-  pixel.setAttribute('cy', cy);
-}
+Nice to have:
+- color palette stays on the page as fixed element, 
+but shrinkable / expandable
 
-var allPixels = document.querySelectorAll('circle');
 
-for (var i = 0; i < allPixels.length; i++) {
-  var p = allPixels[i];
-  var iJewel = Math.floor(i / 7);
-  var iPixel = i % 7;
-
-  p.dataset.iJewel = iJewel;
-  p.dataset.iPixel = iPixel;
-  arrangePixels(p, iJewel, iPixel);
-  p.addEventListener('click', clickSwitchFill);
-  jewels[iJewel].push(p);
-}
-
-function clickSwitchFill(e) {
-  var fill = e.target.getAttribute('fill');
-  if (fill === 'black') e.target.setAttribute('fill', 'white');
-  else if (fill === 'white') e.target.setAttribute('fill', 'black');
-}
-
-function fill() {
-
-}
+*/
